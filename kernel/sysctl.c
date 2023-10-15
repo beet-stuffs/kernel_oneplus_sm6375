c93a0e59c6d1753d539edee021b8300b32d49b0a
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 06:09:31 +0000

    Revert "trace: Toggle irqsoff tracing to dmesg"
    This reverts commit dc28abd85186f16dafa247ef87c1958e23b7f59c.
    
    Change-Id: I3240c4b472db89f152742bda96b49e3608280b71
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -143,9 +143,6 @@ static int six_hundred_forty_kb = 640 * 1024;
 #endif
 static int __maybe_unused max_kswapd_threads = MAX_KSWAPD_THREADS;
 
-static unsigned int __maybe_unused half_million = 500000;
-static unsigned int __maybe_unused one_hundred_million = 100000000;
-
 #ifdef CONFIG_SCHED_WALT
 static int neg_three = -3;
 static int three = 3;
@@ -388,15 +385,6 @@ static struct ctl_table kern_table[] = {
 	{
 		.procname	= "irqsoff_tracing_threshold_ns",
 		.data		= &sysctl_irqsoff_tracing_threshold_ns,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= proc_douintvec_minmax,
-		.extra1		= &half_million,
-		.extra2		= &one_hundred_million,
-	},
-	{
-		.procname	= "irqsoff_dmesg_output_enabled",
-		.data		= &sysctl_irqsoff_dmesg_output_enabled,
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
