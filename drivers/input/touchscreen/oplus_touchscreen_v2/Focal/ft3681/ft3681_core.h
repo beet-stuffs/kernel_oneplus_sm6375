d3fa7e3d52dae7835b67bbea056106fa36a03f81
Sugakesshaa <sugakesshaa@gmail.com>
2024-06-24 04:28:54 +0800

    drivers/input: oplus_touchscreen_v2: Update from OnePlus CPH2381_14.0.0.601(EX01)
    Change-Id: Ic1113750528ffa53c775f76891bebca1336b6d37

--- drivers/input/touchscreen/oplus_touchscreen_v2/Focal/ft3681/ft3681_core.h
+++ drivers/input/touchscreen/oplus_touchscreen_v2/Focal/ft3681/ft3681_core.h
@@ -3,11 +3,10 @@
  * Copyright (C) 2018-2020 Oplus. All rights reserved.
  */
 
-#ifndef __FT3518_CORE_H__
-#define __FT3518_CORE_H__
+#ifndef __FT3681_CORE_H__
+#define __FT3681_CORE_H__
 
 /*********PART1:Head files**********************/
-#include <linux/i2c.h>
 #include "../focal_common.h"
 
 /*********PART2:Define Area**********************/
@@ -18,27 +17,40 @@
 #define INTERVAL_READ_REG                       200  /* unit:ms */
 #define TIMEOUT_READ_REG                        1000 /* unit:ms */
 
-#define FTS_VAL_CHIP_ID                         0x54
-#define FTS_VAL_CHIP_ID2                        0x52
-#define FTS_VAL_BL_ID                           0x54
-#define FTS_VAL_BL_ID2                          0x5C
-
+#define FTS_VAL_CHIP_ID                         0x56
+#define FTS_VAL_CHIP_ID2                        0x62
+#define FTS_VAL_BT_ID                           0x56
+#define FTS_VAL_BT_ID2                          0x62
+#define FTS_VAL_PB_ID                           0x56
+#define FTS_VAL_PB_ID2                          0xE2
 
 #define FTS_REG_POINTS                          0x01
 #define FTS_REG_POINTS_N                        0x10
 #define FTS_REG_POINTS_LB                       0x3E
 
 #define FTS_REG_SMOOTH_LEVEL                    0x85
-#define FTS_REG_GAME_MODE_EN                    0x86
 #define FTS_REG_REPORT_RATE                     0x88/*0x12:180hz, 0x0C:120hz*/
 #define FTS_REG_HIGH_FRAME_TIME                 0x8A
-#define FTS_REG_CHARGER_MODE_EN                 0x8B
-#define FTS_REG_EDGE_LIMIT                      0x8C
 #define FTS_REG_SENSITIVE_LEVEL                 0x90
-#define FTS_REG_HEADSET_MODE_EN                 0xC3
+#define FTS_REG_STABLE_DISTANCE_AFTER_N         0xB9
+#define FTS_REG_STABLE_DISTANCE                 0xBA
 #define FTS_REG_FOD_EN                          0xCF
 #define FTS_REG_FOD_INFO                        0xE1
 #define FTS_REG_FOD_INFO_LEN                    9
+#define FTS_REG_AOD_INFO                        0xD3
+#define FTS_REG_AOD_INFO_LEN                    6
+
+
+#define FTS_REG_GAME_MODE_EN                    0xC3
+#define FTS_REG_CHARGER_MODE_EN                 0x8B
+#define FTS_REG_EDGE_LIMIT                      0x8C
+#define FTS_REG_HEADSET_MODE_EN                 0xC4
+
+#define FTS_REG_CTRL                            0x8B
+#define FTS_REG_CHARGER_MODE_EN_BIT             0x00
+#define FTS_REG_GAME_MODE_EN_BIT                0x02
+#define FTS_REG_EDGE_LIMIT_BIT                  0x04
+#define FTS_REG_HEADSET_MODE_EN_BIT             0x06
 
 #define FTS_REG_INT_CNT                         0x8F
 #define FTS_REG_FLOW_WORK_CNT                   0x91
@@ -63,15 +75,22 @@
 #define FTS_REG_HEALTH_1                        0xFD
 #define FTS_REG_HEALTH_2                        0xFE
 
+#define FTS_REG_WORK_MODE                       0x9E
+#define FTS_REG_WORK_MODE_SNR_MODE              0x81
+#define FTS_REG_WORK_MODE_NORMAL_MODE           0x00
+#define FTS_FW_INFO                             0x96
+#define FTS_REG_TEMPERATURE                     0x97
+
 
 #define FTS_MAX_POINTS_SUPPORT                  10
 #define FTS_MAX_ID                              0x0A
-#define FTS_POINTS_ONE                          15  /*2 + 6*10 + 1*/
-#define FTS_POINTS_TWO                          47  /*8*10 - 1*/
-#define FTS_MAX_POINTS_LENGTH          ((FTS_POINTS_ONE) + (FTS_POINTS_TWO))
+
+#define FTS_MAX_POINTS_LENGTH                   102 /* 2+6*10+4*10 */
+#define FTS_MAX_POINTS_SNR_LENGTH               1505 /* 2+6*10+4*10 + 2 + 2*tx*rx + (tx+rx)*2*2 + 1 + 40 */
+#define FTS_DIFF_BUF_LENGTH                     576 /* tx*rx */
+#define FTS_SC_BUF_LENGTH                       52 /* tx+rx */
 
 #define FTS_GESTURE_DATA_LEN                    28
-
 
 #define BYTES_PER_TIME                          (128)  /* max:128 */
 
@@ -93,15 +112,19 @@
 #define FACTORY_REG_CLB                         0x04
 #define FACTORY_REG_DATA_SELECT                 0x06
 #define FACTORY_REG_FRE_LIST                    0x0A
+#define FACTORY_REG_DATA_TYPE                   0x5B
 #define FACTORY_REG_TOUCH_THR                   0x0D
 #define FACTORY_REG_NORMALIZE                   0x16
 #define FACTORY_REG_MAX_DIFF                    0x1B
 #define FACTORY_REG_FRAME_NUM                   0x1C
+#define FACTORY_REG_GCB                         0xBD
 
 #define FACTORY_REG_RAWDATA_ADDR_MC_SC          0x36
 #define FACTORY_REG_FIR                         0xFB
 #define FACTORY_REG_WC_SEL                      0x09
 #define FACTORY_REG_MC_SC_MODE                  0x44
+#define FACTORY_REG_HC_SEL                      0x0F
+#define FACTORY_REG_MC_SC_CB_H_ADDR_OFF         0x49
 #define FACTORY_REG_MC_SC_CB_ADDR_OFF           0x45
 #define FACTORY_REG_MC_SC_CB_ADDR               0x4E
 #define FACTROY_REG_SHORT_TEST_EN               0x07
@@ -113,6 +136,28 @@
 #define FACTROY_REG_SHORT_DELAY                 0x5A
 #define FACTORY_REG_SHORT_ADDR_MC               0xF4
 
+#define FACTROY_REG_SCAP_CFG                    0x58
+#define FACTROY_REG_SCAP_GCB_TX                 0xBC
+#define FACTROY_REG_SCAP_GCB_RX                 0xBE
+#define FACTROY_REG_CB_BUF_SEL                  0xBF
+
+#define FACTROY_REG_SHORT2_TEST_EN              0xC0
+#define FACTROY_REG_SHORT2_CA                   0x01
+#define FACTROY_REG_SHORT2_CC                   0x02
+#define FACTROY_REG_SHORT2_CG                   0x03
+#define FACTROY_REG_SHORT2_OFFSET               0x04
+#define FACTROY_REG_SHORT2_RES_LEVEL            0xC1
+#define FACTROY_REG_SHORT2_DEALY                0xC2
+#define FACTROY_REG_SHORT2_TEST_STATE           0xC3
+#define FACTORY_REG_SHORT2_ADDR_MC              0xC4
+#define FACTROY_REG_SHORT2_AB_CH                0xC6
+
+#define SC_NUM_MAX                              256
+
+
+#define FACTORY_REG_PARAM_UPDATE_STATE_TOUCH    0xB5
+
+#define FTS_MAX_COMMMAND_LENGTH                 16
 
 #define TEST_RETVAL_00                          0x00
 #define TEST_RETVAL_AA                          0xAA
@@ -124,53 +169,68 @@
 #define FTS_120HZ_REPORT_RATE                   0x0C
 #define FTS_180HZ_REPORT_RATE                   0x12
 
-struct mc_sc_threshold {
-	int noise_coefficient;
-	int short_cg;
-	int short_cc;
-	int *node_valid;
-	int *node_valid_sc;
-	int *rawdata_h_min;
-	int *rawdata_h_max;
-	int *tx_linearity_max;
-	int *tx_linearity_min;
-	int *rx_linearity_max;
-	int *rx_linearity_min;
-	int *scap_cb_off_min;
-	int *scap_cb_off_max;
-	int *scap_cb_on_min;
-	int *scap_cb_on_max;
-	int *scap_rawdata_off_min;
-	int *scap_rawdata_off_max;
-	int *scap_rawdata_on_min;
-	int *scap_rawdata_on_max;
-	int *panel_differ_min;
-	int *panel_differ_max;
-};
+#define FTS_135HZ_REPORT_RATE                   0x00
+#define FTS_240HZ_REPORT_RATE                   0x01
+#define FTS_360HZ_REPORT_RATE                   0x02
+#define FTS_720HZ_REPORT_RATE                   0x03
 
-struct fts_test {
-	struct mc_sc_threshold thr;
-};
+#define FTS_GET_RATE_180                        180
+#define FTS_GET_RATE_300                        300
+#define FTS_GET_RATE_600                        600
+
+#define SPI_RETRY_NUMBER          				3
+#define CS_HIGH_DELAY             				150 /* unit: us */
+#define SPI_BUF_LENGTH          			    4096
+
+#define DATA_CRC_EN               				0x20
+#define WRITE_CMD                 				0x00
+#define READ_CMD                    			(0x80 | DATA_CRC_EN)
+
+#define SPI_DUMMY_BYTE             				3
+#define SPI_HEADER_LENGTH           			6   /*CRC*/
+
+#define PITCH_X_WIDTH 540
+#define PITCH_Y_WIDTH 536
+#define PITCH_PHY_WIDTH_MM 4
+
+#define GET_LEN_BY_WIDTH_MAJOR(width_major, len)\
+({\
+	if (width_major > 10 && width_major < 14)\
+		*len = 5;\
+	if (width_major > 12 && width_major < 16)\
+		*len = 5;\
+	if (width_major > 16 && width_major < 20)\
+		*len = 7;\
+	if (width_major > 18 && width_major < 22)\
+		*len = 7;\
+})
 
 struct fts_autotest_offset {
 	int32_t *fts_raw_data_P;
 	int32_t *fts_raw_data_N;
+	int32_t *fts_panel_differ_data_P;
+	int32_t *fts_panel_differ_data_N;
 	int32_t *fts_noise_data_P;
 	int32_t *fts_noise_data_N;
 	int32_t *fts_uniformity_data_P;
 	int32_t *fts_uniformity_data_N;
 	int32_t *fts_scap_cb_data_P;
 	int32_t *fts_scap_cb_data_N;
-	int32_t *fts_scap_raw_data_P;
-	int32_t *fts_scap_raw_data_N;
 	int32_t *fts_scap_cb_data_waterproof_P;
 	int32_t *fts_scap_cb_data_waterproof_N;
-	int32_t *fts_panel_differ_data_P;
-	int32_t *fts_panel_differ_data_N;
+	int32_t *fts_scap_raw_data_P;
+	int32_t *fts_scap_raw_data_N;
 	int32_t *fts_scap_raw_waterproof_data_P;
 	int32_t *fts_scap_raw_waterproof_data_N;
 };
 
+enum FW_STATUS {
+	FTS_RUN_IN_ERROR,
+	FTS_RUN_IN_APP,
+	FTS_RUN_IN_ROM,
+	FTS_RUN_IN_PRAM,
+	FTS_RUN_IN_BOOTLOADER,
+};
 
 struct fts_fod_info {
 	u8 fp_id;
@@ -183,18 +243,38 @@ struct fts_fod_info {
 	u8 fp_down_report;
 };
 
-struct chip_data_ft3518 {
+struct fts_aod_info {
+	u8 gesture_id;
+	u8 point_num;
+	u16 aod_x;
+	u16 aod_y;
+};
+
+struct ftxxxx_proc {
+	struct proc_dir_entry *proc_entry;
+	u8 opmode;
+	u8 cmd_len;
+	u8 cmd[FTS_MAX_COMMMAND_LENGTH];
+};
+
+struct chip_data_ft3681 {
 	bool esd_check_need_stop;   /*true:esd check do nothing*/
 	bool esd_check_enabled;
 	bool use_panelfactory_limit;
 	bool is_power_down;
 	bool is_ic_sleep;    /*ic sleep status*/
-	u8 rbuf[FTS_MAX_POINTS_LENGTH];
+	u8 touch_buf[FTS_MAX_POINTS_SNR_LENGTH];
 	u8 irq_type;
-	u8 fwver;
 	u8 touch_direction;
 	u8 fp_en;
 	u8 fp_down;
+	u8 fwver;
+	u32 touch_size;
+
+	u8 snr_buf[FTS_MAX_POINTS_SNR_LENGTH];
+	int diff_buf[FTS_DIFF_BUF_LENGTH];
+	int sc_water[FTS_SC_BUF_LENGTH];
+	int sc_nomal[FTS_SC_BUF_LENGTH];
 
 	int rl_cnt;
 	int scb_cnt;
@@ -205,54 +285,80 @@ struct chip_data_ft3518 {
 	int probe_done;
 	int *noise_rawdata;
 	int *rawdata;
+	int *panel_differ;
 	int *scap_cb;
 	int *scap_rawdata;
-	int *panel_differ_raw;
 	int *rawdata_linearity;
 	int tp_index;
+	u8 fre_num;
+	u8 ctrl_reg_state;
+	u8 snr_count;
+	int tp_temperature;
+
+	int scap_gcb_rx;
+	int scap_gcb_tx;
 
 	char *test_limit_name;
 	char *fw_name;
 
+	unsigned char *bus_tx_buf;
+	unsigned char *bus_rx_buf;
+
 	tp_dev tp_type;             /*tp_devices.h*/
 
-	struct i2c_client *client;
+	struct spi_device *ft_spi;
 	struct device *dev;
 	struct hw_resource *hw_res;
 	struct fts_proc_operations *syna_ops;
 	struct fts_fod_info fod_info;
+	struct fts_aod_info aod_info;
 	struct seq_file *s;
-	struct fts_test mpt;
 	struct fts_autotest_offset *fts_autotest_offset;
 	struct touchpanel_data *ts;
+	struct mutex bus_lock;
+	struct ftxxxx_proc proc;
+
+	int *node_valid;
+	int *node_valid_sc;
 	int gesture_state;
 	bool black_gesture_indep;
+	bool switch_game_rate_support;
 	bool high_resolution_support;
 	bool high_resolution_support_x8;
-	bool read_buffer_support;
+	bool snr_is_reading;
+	bool snr_read_support;
+	bool snr_data_is_ready;
 };
 
 
-extern struct chip_data_ft3518 *g_fts_data;
+extern struct chip_data_ft3681 *g_fts3681_data;
 
-int fts_test_entry(struct chip_data_ft3518 *ts_data,
+int focal_create_apk_debug_channel(struct touchpanel_data *ts);
+int fts3681_test_entry(struct chip_data_ft3681 *ts_data,
 		   struct auto_testdata *focal_testdata);
-int ft3518_auto_preoperation(struct seq_file *s, void *chip_data,
+int ft3681_auto_preoperation(struct seq_file *s, void *chip_data,
 			     struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
-int ft3518_noise_autotest(struct seq_file *s, void *chip_data,
+int ft3681_noise_autotest(struct seq_file *s, void *chip_data,
 			  struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
-int ft3518_rawdata_autotest(struct seq_file *s, void *chip_data,
+int ft3681_rawdata_autotest(struct seq_file *s, void *chip_data,
 			    struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
-int ft3518_uniformity_autotest(struct seq_file *s, void *chip_data,
+int ft3681_uniformity_autotest(struct seq_file *s, void *chip_data,
 			       struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
-int ft3518_scap_cb_autotest(struct seq_file *s, void *chip_data,
+int ft3681_scap_cb_autotest(struct seq_file *s, void *chip_data,
 			    struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
-int ft3518_scap_rawdata_autotest(struct seq_file *s, void *chip_data,
+int ft3681_scap_rawdata_autotest(struct seq_file *s, void *chip_data,
 				 struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
-int ft3518_short_test(struct seq_file *s, void *chip_data,
+int ft3681_short_test(struct seq_file *s, void *chip_data,
 		      struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
-int ft3518_auto_endoperation(struct seq_file *s, void *chip_data,
+int ft3681_panel_differ_test(struct seq_file *s, void *chip_data,
+                             struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
+int ft3681_auto_endoperation(struct seq_file *s, void *chip_data,
 			     struct auto_testdata *focal_testdata, struct test_item_info *p_test_item_info);
 
+int ft3681_fts_read(u8 *cmd, u32 cmdlen, u8 *data, u32 datalen);
+int ft3681_fts_write(u8 *writebuf, u32 writelen);
+int ft3681_fts_write_reg(u8 addr, u8 value);
+int ft3681_fts_read_reg(u8 addr, u8 *value);
 
-#endif /*__FT3518_CORE_H__*/
+
+#endif /*__FT3681_CORE_H__*/
